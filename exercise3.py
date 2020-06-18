518114bb40756cec7bf473c1f3f5790a6a10452e
JakubSapko <jakub.sapko1@gmail.com>
2020-06-18 15:27:38 +0200

    solve exercise 3

--- exercise3.py
+++ exercise3.py
@@ -6,17 +6,36 @@
 # Drugi set ma zawierac liczyb mniejsze od n, których modulo 3 daje 2
 
 
-even = set()
-less = set()
+A = set()
+B = set()
 
 i = 0
 n = int(input("Podaj liczbę n\n"))
 while i < n:
     if i%2 == 0:
-        even.add(i)
+        A.add(i)
     if i%3 == 2:
-        less.add(i)
+        B.add(i)
     i+=1
 
-print(even)
-print(less)
+print(A)
+print(B)
+
+C = A.union(B)
+print(C)
+print(len(C))
+# wtf is A*B
+
+E = A.difference(B)
+print(E)
+print(len(E))
+
+
+F = B^A
+print(F)
+print(len(F))
+
+if B.issubset(A):
+    print("B zawiera sie w A")
+else:
+    print("B nie zawiera sie w A")
