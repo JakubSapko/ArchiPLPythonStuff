c130334f56bb0be17656b3b4e5637a173f430e22
JakubSapko <jakub.sapko1@gmail.com>
2020-06-18 14:49:18 +0200

    solve second exercise

--- skryptagata.py
+++ skryptagata.py
@@ -31,4 +31,31 @@ print(set)
 
 # ---- Zadanie 2 ----
 
-A = {'aksamitka', 'bratek', 'cynia', 'kosmos'}
+A = {'aksamitka', 'bratek', 'cynia', 'kosmos', 'aster', 'sasanka', 'zawilec', 'konwalia', 'rumianek', 'tulipan', 'hiacynt', 'narcyz'}
+B = {'bratek', 'pierwiosnek', 'przylaszczka', 'cebulica', 'zawilec', 'krokus', 'sasanka'}
+print(A)
+print(B)
+
+
+# Set C zawiera elementy A lub B
+C = A.union(B)
+#print(C)
+print(len(C))
+
+# Set D zawiera elementy przecięcia A i B
+
+D = A.intersection(B)
+#print(D)
+print(len(D))
+
+# Set E zawiera elementy, które należą do A, ale nie do B
+
+E = A.difference(B)
+#print(E)
+print(len(E))
+
+# Set F zawiera elementy, które należą do B, ale nie do A
+
+F = B.difference(A)
+#print(F)
+print(len(F))
